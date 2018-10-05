57db60057333a808b9cf55fefd1fea06447eaed9
ramya257 <educate1996@gmail.com>
2018-10-05 18:01:53 +0530

    modified files

--- MovieWebApplication/src/com/cg/movie/bean/Category.java
+++ MovieWebApplication/src/com/cg/movie/bean/Category.java
@@ -1,24 +1,32 @@
 package com.cg.movie.bean;
 
 import javax.persistence.Entity;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 
 @Entity
 @Table(name="movie_category")
 public class Category {
+	
+	@Id
 	private int categoryId;
-	private int categoryName;
+	private String categoryName;
+	public Category(int categoryId, String categoryName) {
+		super();
+		this.categoryId = categoryId;
+		this.categoryName = categoryName;
+	}
 	public int getCategoryId() {
 		return categoryId;
 	}
 	public void setCategoryId(int categoryId) {
 		this.categoryId = categoryId;
 	}
-	public int getCategoryName() {
+	public String getCategoryName() {
 		return categoryName;
 	}
-	public void setCategoryName(int categoryName) {
+	public void setCategoryName(String categoryName) {
 		this.categoryName = categoryName;
 	}
 	@Override
